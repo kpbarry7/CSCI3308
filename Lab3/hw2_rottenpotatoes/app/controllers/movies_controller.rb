15e733d089783f400907e7e9efb4fc8912fdbced
kpbarry7 <kevin.p.barry@colorado.edu>
2013-10-08 19:49:21 -0600

    Almost there

--- Lab3/hw2_rottenpotatoes/app/controllers/movies_controller.rb
+++ Lab3/hw2_rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,7 +7,42 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.ratings
+
+    # If there are parameters add them to the session
+    if params[:sort]
+      session[:sort] = params[:sort]
+    end
+
+    if params[:ratings]
+      session[:ratings] = params[:ratings]
+    end
+    
+    if session[:sort] == nil
+      @sort = ''
+    else
+      @sort = session[:sort]
+    end
+
+    if session[:ratings].class.name.include?('Hash')
+      @ratings = session[:ratings].keys
+    else
+      if session[:ratings] == nil
+        @ratings = @all_ratings
+      else
+        @ratings = session[:ratings]
+      end
+    end
+
+    if session[:sort] == 'title'
+      @title_header = 'hilite'
+    elsif session[:sort] == 'release_date DESC'
+      @release_date_header = 'hilite'
+    end
+
+    @movies = Movie.where(:rating => @ratings).order(@sort)
+
+    redirect_to movies_path(:sort => @sort, :ratings => @ratings) unless (params[:sort] || params[:ratings])
   end
 
   def new
@@ -37,5 +72,4 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
 end
